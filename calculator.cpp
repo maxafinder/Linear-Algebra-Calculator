aaf9ccc026d413a466a9a047cd1ab2be3981171e
Max Finder <mfinder@purdue.edu>
2021-06-16 17:15:32 -0500

    Added function getDimensions() to calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+// function prototypes
+int * getDimensions();
+
+
+
 int main() {
 
+	// get dimensions of the matrix
+	int * dimensions = getDimensions();
 
 } // main()
+
+
+
+
+
+
+
+/*
+ * Gets the dimensions of the matrix from the user.
+ * @return -> An int array called "dimensions" where dimensions[0] is the # of rows,
+ * 						and dimensions[1] is the # of columns
+ */
+int * getDimensions() {
+	int * dimensions = new int[2];
+
+	printf("Enter the number of rows:  ");
+	std::cin >> dimensions[0];
+
+	printf("Enter the number of columns:  ");
+	std::cin >> dimensions[1];
+
+	return dimensions;
+} // getDimensions()
